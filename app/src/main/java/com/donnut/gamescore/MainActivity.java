cc1f3f5f89258186ad81b853b9de940b49b05015
fdonnut <36168787+fdonnut@users.noreply.github.com>
2022-11-24 22:19:52 +0300

    logs onStart onStop

--- app/src/main/java/com/donnut/gamescore/MainActivity.java
+++ app/src/main/java/com/donnut/gamescore/MainActivity.java
@@ -1,6 +1,7 @@
 package com.donnut.gamescore;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,24 +12,43 @@ public class MainActivity extends AppCompatActivity {
     private int score1 = 0;
     private int score2 = 0;
 
+    private TextView textViewScore1;
+    private TextView textViewScore2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("MainActivity", "onCreate");
+
+        textViewScore1 = findViewById(R.id.textViewScore1);
+        textViewScore2 = findViewById(R.id.textViewScore2);
 
         if (savedInstanceState != null) {
             score1 = savedInstanceState.getInt("score1");
             score2 = savedInstanceState.getInt("score2");
         }
 
-        TextView textViewScore1 = findViewById(R.id.textViewScore1);
-        TextView textViewScore2 = findViewById(R.id.textViewScore2);
+        updateScore1();
+        updateScore2();
 
-        textViewScore1.setText(String.valueOf(score1));
-        textViewScore2.setText(String.valueOf(score2));
+        textViewScore1.setOnClickListener(view -> {
+            Log.d("MainActivity", "Clicked textViewScore1");
+            score1++;
+            updateScore1();
+        });
+        textViewScore2.setOnClickListener(view -> {
+            Log.d("MainActivity", "Clicked textViewScore2");
+            score2++;
+            updateScore2();
+        });
+    }
 
-        textViewScore1.setOnClickListener(view -> textViewScore1.setText(String.valueOf(++score1)));
-        textViewScore2.setOnClickListener(view -> textViewScore2.setText(String.valueOf(++score2)));
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("MainActivity", "onStart");
     }
 
     @Override
@@ -38,8 +58,23 @@ public class MainActivity extends AppCompatActivity {
         outState.putInt("score2", score2);
     }
 
+    private void updateScore1() {
+        textViewScore1.setText(String.valueOf(score1));
+    }
+
+    private void updateScore2() {
+        textViewScore2.setText(String.valueOf(score2));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MainActivity", "onStop");
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d("MainActivity", "onDestroy");
     }
 }
